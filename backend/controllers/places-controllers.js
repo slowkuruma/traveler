53598aa3b397fca64d8586609171f1bad4aeb549
slowkuruma <stevan.han82@gmail.com>
2020-09-18 21:43:11 -0700

    bug fix

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
-
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
 const Place = require('../models/place');
