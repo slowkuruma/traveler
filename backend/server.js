07c9ec99c6b7c42a02db78bfb6a6dd5a42771ceb
slowkuruma <stevan.han82@gmail.com>
2020-09-19 15:36:37 -0700

    deadspace removed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
